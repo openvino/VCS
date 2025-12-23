df47aee7063395d02b33b8cccacdeacb0a20aa81
Pablo Levy <pabloele@yankee.openvino.org>
2025-12-23 08:14:17 +0000

    issuance profile

--- vcs/tools/didconfig/main.go
+++ vcs/tools/didconfig/main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/trustbloc/did-go/doc/did"
 	utiltime "github.com/trustbloc/did-go/doc/util/time"
 	vdrapi "github.com/trustbloc/did-go/vdr/api"
+	kmsjwk "github.com/trustbloc/kms-go/doc/jose/jwk"
 	kmsapi "github.com/trustbloc/kms-go/spi/kms"
 	"github.com/trustbloc/vc-go/verifiable"
 
@@ -18,7 +19,6 @@ import (
 	"github.com/trustbloc/vcs/pkg/doc/vc/crypto"
 	vcsverifiable "github.com/trustbloc/vcs/pkg/doc/verifiable"
 	"github.com/trustbloc/vcs/pkg/kms"
-	"github.com/trustbloc/vcs/pkg/service/didconfiguration"
 )
 
 const (
@@ -34,14 +34,17 @@ func main() {
 		verificationMethod = "did:ion:bank_issuer#key-id"
 		domain             = "https://yankee.openvino.org"
 		stagingFile        = "/var/www/VCS/.well-known/did-configuration.json"
+		didDocFile         = "/var/www/VCS/vcs/test/bdd/fixtures/file-server/dids/did-ion-bank-issuer.json"
 	)
 
-	cred, err := createLinkedDomainCredential(issuerDID, verificationMethod, domain)
+	cred, jwk, err := createLinkedDomainCredential(issuerDID, verificationMethod, domain)
 	exitOnErr("create credential", err)
 
-	output := &didconfiguration.DidConfiguration{
-		Context:    didConfigurationContextURL,
-		LinkedDiDs: []interface{}{cred.JWTEnvelope.JWT},
+	output := map[string]interface{}{
+		"@context": []string{didConfigurationContextURL},
+		"linked_dids": []interface{}{
+			cred.JWTEnvelope.JWT,
+		},
 	}
 
 	bytes, err := json.MarshalIndent(output, "", "  ")
@@ -49,20 +52,29 @@ func main() {
 
 	exitOnErr("write staging file", writeFile(stagingFile, bytes))
 	log.Printf("did-configuration written to %s\n", stagingFile)
+
+	doc, err := buildDIDDocument(issuerDID, verificationMethod, domain, jwk)
+	exitOnErr("build did doc", err)
+
+	docBytes, err := json.MarshalIndent(doc, "", "  ")
+	exitOnErr("marshal did doc", err)
+
+	exitOnErr("write did doc", writeFile(didDocFile, docBytes))
+	log.Printf("did document written to %s\n", didDocFile)
 }
 
-func createLinkedDomainCredential(issuerDID, verificationMethod, domain string) (*verifiable.Credential, error) {
+func createLinkedDomainCredential(issuerDID, verificationMethod, domain string) (*verifiable.Credential, *kmsjwk.JWK, error) {
 	km, err := createLocalKMS()
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	doc := newStaticDIDDoc(issuerDID, verificationMethod)
 	cryptoSvc := crypto.New(&staticVDR{doc: doc}, nil)
 
-	keyID, _, err := km.CreateJWKKey(kmsapi.ED25519Type)
+	keyID, pubJWK, err := km.CreateJWKKey(kmsapi.ED25519Type)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	signer := &vc.Signer{
@@ -79,10 +91,15 @@ func createLinkedDomainCredential(issuerDID, verificationMethod, domain string) 
 
 	unsignedVC, err := verifiable.CreateCredential(contents, nil)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	return cryptoSvc.SignCredential(signer, unsignedVC)
+	signed, err := cryptoSvc.SignCredential(signer, unsignedVC)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return signed, pubJWK, nil
 }
 
 func createLocalKMS() (*kms.KeyManager, error) {
@@ -108,9 +125,7 @@ func buildCredentialContents(issuerDID, domain string) verifiable.CredentialCont
 			vcTypeVerifiableCredential,
 			vcTypeDomainLinkageCredential,
 		},
-		Issuer: &verifiable.Issuer{
-			ID: issuerDID,
-		},
+		Issuer: &verifiable.Issuer{ID: issuerDID},
 		Subject: []verifiable.Subject{{
 			ID: issuerDID,
 			CustomFields: map[string]interface{}{
@@ -120,6 +135,96 @@ func buildCredentialContents(issuerDID, domain string) verifiable.CredentialCont
 		Issued:  utiltime.NewTime(time.Now().UTC()),
 		Expired: utiltime.NewTime(time.Now().UTC().Add(365 * 24 * time.Hour)),
 	}
+}
+
+func buildDIDDocument(issuerDID, verificationMethod, domain string, jwk *kmsjwk.JWK) (*didDocument, error) {
+	jwkBytes, err := jwk.MarshalJSON()
+	if err != nil {
+		return nil, err
+	}
+
+	var jwkData map[string]interface{}
+	if err = json.Unmarshal(jwkBytes, &jwkData); err != nil {
+		return nil, err
+	}
+
+	return &didDocument{
+		Context: []string{"https://www.w3.org/ns/did/v1"},
+		ID:      issuerDID,
+		VerificationMethod: []verificationMethodEntry{{
+			ID:           verificationMethod,
+			Type:         "JsonWebKey2020",
+			Controller:   issuerDID,
+			PublicKeyJWK: jwkData,
+		}},
+		Authentication:  []string{verificationMethod},
+		AssertionMethod: []string{verificationMethod},
+		Service: []serviceEntry{{
+			ID:              issuerDID + "#linked-domain",
+			Type:            "LinkedDomains",
+			ServiceEndpoint: domain + "/.well-known/did-configuration.json",
+		}},
+	}, nil
+}
+
+type didDocument struct {
+	Context            []string                  `json:"@context"`
+	ID                 string                    `json:"id"`
+	VerificationMethod []verificationMethodEntry `json:"verificationMethod"`
+	Authentication     []string                  `json:"authentication"`
+	AssertionMethod    []string                  `json:"assertionMethod"`
+	Service            []serviceEntry            `json:"service"`
+}
+
+type verificationMethodEntry struct {
+	ID           string                 `json:"id"`
+	Type         string                 `json:"type"`
+	Controller   string                 `json:"controller"`
+	PublicKeyJWK map[string]interface{} `json:"publicKeyJwk,omitempty"`
+}
+
+type serviceEntry struct {
+	ID              string `json:"id"`
+	Type            string `json:"type"`
+	ServiceEndpoint string `json:"serviceEndpoint"`
+}
+
+type staticVDR struct {
+	doc *did.Doc
+}
+
+func (s *staticVDR) Resolve(string, ...vdrapi.DIDMethodOption) (*did.DocResolution, error) {
+	return &did.DocResolution{DIDDocument: s.doc}, nil
+}
+func (s *staticVDR) Create(string, *did.Doc, ...vdrapi.DIDMethodOption) (*did.DocResolution, error) {
+	return nil, errors.New("not supported")
+}
+func (s *staticVDR) Update(*did.Doc, ...vdrapi.DIDMethodOption) error {
+	return errors.New("not supported")
+}
+func (s *staticVDR) Deactivate(string, ...vdrapi.DIDMethodOption) error {
+	return errors.New("not supported")
+}
+func (s *staticVDR) Close() error { return nil }
+
+func newStaticDIDDoc(issuerDID, verificationMethod string) *did.Doc {
+	vm := did.VerificationMethod{
+		ID:         verificationMethod,
+		Type:       "JsonWebKey2020",
+		Controller: issuerDID,
+	}
+
+	doc := &did.Doc{
+		ID:                 issuerDID,
+		VerificationMethod: []did.VerificationMethod{vm},
+	}
+
+	doc.Authentication = []did.Verification{{
+		VerificationMethod: vm,
+		Relationship:       did.Authentication,
+	}}
+
+	return doc
 }
 
 func getenvOrFail(name string) string {
@@ -141,49 +246,5 @@ func writeFile(path string, data []byte) error {
 	if err := os.MkdirAll(dir, 0o755); err != nil {
 		return err
 	}
-
 	return os.WriteFile(path, data, 0o644)
 }
-
-type staticVDR struct {
-	doc *did.Doc
-}
-
-func (s *staticVDR) Resolve(string, ...vdrapi.DIDMethodOption) (*did.DocResolution, error) {
-	return &did.DocResolution{DIDDocument: s.doc}, nil
-}
-
-func (s *staticVDR) Create(string, *did.Doc, ...vdrapi.DIDMethodOption) (*did.DocResolution, error) {
-	return nil, errors.New("not supported")
-}
-
-func (s *staticVDR) Update(*did.Doc, ...vdrapi.DIDMethodOption) error {
-	return errors.New("not supported")
-}
-
-func (s *staticVDR) Deactivate(string, ...vdrapi.DIDMethodOption) error {
-	return errors.New("not supported")
-}
-
-func (s *staticVDR) Close() error { return nil }
-
-func newStaticDIDDoc(issuerDID, verificationMethod string) *did.Doc {
-	vm := did.VerificationMethod{
-		ID:         verificationMethod,
-		Type:       crypto.Ed25519VerificationKey2018,
-		Controller: issuerDID,
-	}
-
-	doc := &did.Doc{
-		ID:                 issuerDID,
-		VerificationMethod: []did.VerificationMethod{vm},
-	}
-
-	doc.Authentication = []did.Verification{{
-		VerificationMethod: vm,
-		Relationship:       did.Authentication,
-	}}
-
-	return doc
-}
-
